2b283973f9d034706ce2f554c028c7c428df40c9
Zac Guo <zacharyg@usc.edu>
2016-10-19 20:53:25 -0700

    Initial Commit

---
+++ TwitterCloneTests/TwitterCloneTests.swift
@@ -0,0 +1,36 @@
+//
+//  TwitterCloneTests.swift
+//  TwitterCloneTests
+//
+//  Created by Zachary West Guo on 10/19/16.
+//  Copyright © 2016 zechariah. All rights reserved.
+//
+
+import XCTest
+@testable import TwitterClone
+
+class TwitterCloneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
